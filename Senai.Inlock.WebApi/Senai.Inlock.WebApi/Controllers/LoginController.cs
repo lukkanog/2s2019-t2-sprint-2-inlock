1ca8fa260a5e84681017447f8296ef5b2bd7ed58
Lucas Nogueira De Souza <46735197879@info.local>
2019-09-02 08:55:48 -0300

    :hatching_chick:

---
+++ Senai.Inlock.WebApi/Senai.Inlock.WebApi/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Senai.Inlock.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login()
+        {
+
+        }
+
+    }
+}
